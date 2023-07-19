82dde4d45fe8de178534d413c69ebe2fca98bf7d
JunioR <donci@internode.hu>
2023-07-19 11:17:55 +0200

    valami

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
   import { ref } from 'vue';
   const count = ref(0)
-  const attributs = {
+  /* const attributs = {
     id: "valami",
     class: "öt"
   } 
@@ -14,8 +14,31 @@
       status.value = ""  
     }
     console.log("toggle")
-  }
+  } */
+  /* function toggle(){}
+  const valtozo = ref("walami") */
 
+ /*  function render(){
+    const newH1 = document.createElement("h1");
+    const newH1Content = document.createTextNode("Jó napot");
+    newH1.appendChild(newH1Content);
+
+    const newP = document.createElement("p");
+    const newPContent = document.createTextNode("egy két há");
+    newP.appendChild(newPContent);
+
+    const a = document.createAttribute("class");
+    a.value = valtozo;
+    newP.setAttribute(a);
+    newP.addEventListener('click', toggle);
+
+    const newDiv = document.createElement("div");
+    newDiv.appendChild(newH1);
+    newDiv.appendChild(newP);
+
+    const currentDiv = document.getElementById("div1");
+    document.body.insertBefore(newDiv, currentDiv);
+  }  */
 </script>
 
 <template>
@@ -26,15 +49,20 @@
     <p>Lorem ipsum</p>
   </div>
   <div>
-    <!-- <input type="text" :value="count" v-on:input="event => count = event.target.value"> -->
+    
     <input type="text" v-model="count">
     <p> {{count}} </p>
-    <button @click="count++">  <!-- @ ==> v-on: -->
+    <button @click="count++">  
       increment
     </button> 
   </div>
+  <div>
+    <h1>Jó napot</h1>
+    <p>egy két há</p>
+  </div>
 </template>
-
+<!-- @ ==> v-on: -->
+<!-- <input type="text" :value="count" v-on:input="event => count = event.target.value"> -->
 <style scoped>
   .box p{
     color: red;
@@ -44,3 +72,4 @@
     display: none;
   }
 </style>
+<!-- HF: 1 Legyen egy olyan div amiben van h1 és p 2. másik div amiben egy sima szöveg doboz van meg egy textarea -->
